2024ba7e6b921c3f817647a601ca76b217b67580
Facundo Munoz <facundomartinmunoz@gmail.com>
2024-01-23 09:41:28 +0800

    branding

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,20 +1,18 @@
 "use client"
 
-import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
 import { IconArrowRight } from "@tabler/icons-react"
-import { useTheme } from "next-themes"
 import Link from "next/link"
+import Image from "next/image"
+import ToptivePng from "@/components/icons/toptive.png"
 
 export default function HomePage() {
-  const { theme } = useTheme()
-
   return (
     <div className="flex size-full flex-col items-center justify-center">
       <div>
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
+        <Image src={ToptivePng} width={200} height={200} alt="toptive logo" />
       </div>
 
-      <div className="mt-2 text-4xl font-bold">Chatbot UI</div>
+      <div className="text-4xl font-bold">ChatGPT</div>
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
--- components/ui/brand.tsx
+++ components/ui/brand.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import Link from "next/link"
+import Image from "next/image"
 import { FC } from "react"
-import { ChatbotUISVG } from "../icons/chatbotui-svg"
+import ToptivePng from "../icons/toptive.png"
 
 interface BrandProps {
   theme?: "dark" | "light"
@@ -12,15 +13,15 @@ export const Brand: FC<BrandProps> = ({ theme = "dark" }) => {
   return (
     <Link
       className="flex cursor-pointer flex-col items-center hover:opacity-50"
-      href="https://www.chatbotui.com"
+      href="https://chatgpt.toptive.dev"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <div className="mb-2">
-        <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
+      <div className="">
+        <Image src={ToptivePng} width={200} height={200} alt="toptive logo" />
       </div>
 
-      <div className="text-4xl font-bold tracking-wide">Chatbot UI</div>
+      <div className="text-4xl font-bold tracking-wide">ChatGPT</div>
     </Link>
   )
 }
