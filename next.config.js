7a1a90f49184cbf65194c347dd5f52adc47656b6
Facundo Munoz <facundomartinmunoz@gmail.com>
2024-04-29 21:38:26 +0800

    disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,32 +1,38 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true"
+// })
 
-const withPWA = require("next-pwa")({
-  dest: "public"
-})
+// const withPWA = require("next-pwa")({
+//   dest: "public"
+// })
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
+// module.exports = withBundleAnalyzer(
+//   withPWA({
+//     reactStrictMode: true,
+//     images: {
+//       unoptimized: true,
+//       remotePatterns: [
+//         {
+//           protocol: "http",
+//           hostname: "localhost"
+//         },
+//         {
+//           protocol: "http",
+//           hostname: "127.0.0.1"
+//         },
+//         {
+//           protocol: "https",
+//           hostname: "**"
+//         }
+//       ]
+//     },
+//     experimental: {
+//       serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
+//     }
+//   }),
+// )
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+};
